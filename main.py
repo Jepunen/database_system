c9635dae81f4325918ee68cade8a3503a4cf740d
Jepunen <62857023+Jepunen@users.noreply.github.com>
2023-03-08 21:44:17 +0200

    DB works

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import sqlite3
 
+def main():
+    db_name = 'database.sqlite'
+    conn = connectToDB(db_name)
+
+    create_student(conn, ("Kalle",))
+
+    return None
+
+
 def connectToDB(db_name):
     conn = None
     try:
@@ -9,3 +18,19 @@ def connectToDB(db_name):
         print(e)
     return conn
 
+
+def create_student(conn, student):
+    sql = ''' INSERT INTO Student(FirstName)
+              VALUES(?) '''
+    cur = conn.cursor()
+    cur.execute(sql, student)
+    conn.commit()
+
+    everything = cur.execute('SELECT * FROM Student')
+    for row in everything:
+        print(row[0])
+    print(everything)
+
+    return cur.lastrowid
+
+main()
